a15e0cd343242cba67f018bac67915f7f0fd5b42
kayxom <143403330+kayxom@users.noreply.github.com>
2024-02-25 12:52:39 -0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Pessoa p1 = new Pessoa();
+        p1.Peso= 50.20;
+        p1.altura= 1.73;
+        p1.nome= "kayke";
+        p1.ano= 15;
+        p1.mostrardados();
+      
+    }
+
+}
